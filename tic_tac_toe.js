1c2d816dcc0c81049fbe370bd63aac78baf1fe5f
michael gombos <gombos2002@gmail.com>
2020-10-20 19:23:12 -0500

    created win loss logic, added draw check

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,19 +1,83 @@
 
+const boardLogic = (() => {
+    //keep track of winner mark!
+    const checkDiagonalWin = (board) => {
+        console.table(board);
+        if(board[1][1] != "" && 
+        ( (board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+        (board[0][2] == board[1][1] && board[1][1] == board[2][0]) )){
+            return true;
+        }
+        return false;
+    }
+
+    const checkVerticalWin = (board) => {
+        for(let i = 0; i < 3; i++){
+            if( (board[1][i] != "") &&
+            (board[0][i] == board[1][i] && board[1][i] == board[2][i])){
+                console.log("vert win");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkHorizantalWin = (board) => {
+        for(let i = 0; i < 3; i++){
+            if( (board[i][1] != "") &&
+            (board[i][0] == board[i][1] && board[i][1] == board[i][2])){
+                console.log("hoz win");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkBoardFull = (board) => {
+        for(let i = 0; i < 3; i++){
+            if(board[0][i] == "" || board[1][i] == "" || board[2][i] == ""){
+                //if one piece is empty board not full
+                console.log("board not full")
+                return false;
+            }
+            else{
+                console.log("boardFull");
+                console.log("draw!!!")
+                return true;
+            }
+        }
+    }
+    const checkWin = (board) =>{
+        const gameBoard = board;
+        if(checkDiagonalWin(gameBoard) || checkVerticalWin(gameBoard)
+        || checkHorizantalWin(gameBoard)){
+            return true;
+        }
+    }
+
+    return{checkWin, checkBoardFull};
+})();
+
 const Gameboard = (() => {
     const gameboard = [["","",""],["","",""],["","",""]];
 
     const addMark = (mark,spot) => {
-        console.log(spot);
-        console.log(mark);
+
         let markIndex = spot.split(" ");
         if(gameboard[markIndex[0]][markIndex[1]] == ""){
             gameboard[markIndex[0]][markIndex[1]] = mark;
-            // console.log(`placed ${mark}`);
+
         }
         else{
-            // console.log(`spot ${markIndex} already taken`);
-            //TODO-- signal spot taken!
+
+            // alert("spot taken!");
         }
+
+        if(boardLogic.checkWin(gameboard)){
+            console.log(`${mark} won game`);
+            //TODO update score
+        } 
+
     }
 
     const display = () =>{
@@ -28,10 +92,6 @@ const Player = (name,mark) =>{ //'mark' X or O
     const getMark = () => mark;
 
     const placeMark = spot => { 
-        //spot index's for row and column
-        //ex spot = '0 0'
-        //must be able to avoid placing mark when another mark 
-        //already exists!
         Gameboard.addMark(mark, spot);
     }
     return{placeMark};
@@ -40,6 +100,7 @@ const Player = (name,mark) =>{ //'mark' X or O
 
 const mike = Player("mike","X");
 const bob = Player("bob","O");
-mike.placeMark('1 0');
-bob.placeMark('1 1');
-Gameboard.display();
+bob.placeMark("0 1");
+bob.placeMark("1 1");
+bob.placeMark("1 0");
+bob.placeMark("1 2");
